725caa562adfafad481e24f99d012060cccd266d
Alan Shaw <alan@tableflip.io>
2016-10-17 18:25:44 +0100

    Basic HTML and functionality for navbar

--- imports/ui/layout.jsx
+++ imports/ui/layout.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react'
 import { Meteor } from 'meteor/meteor'
 import { createContainer } from 'meteor/react-meteor-data'
 import Authenticator from './users/authenticator'
+import NavBar from './navigation/navbar'
 
 const Layout = React.createClass({
   propTypes: {
@@ -9,9 +10,19 @@ const Layout = React.createClass({
   },
 
   render () {
-    const { children, user } = this.props
-    return <Authenticator user={user}>{children}</Authenticator>
+    const { children, user, notifications } = this.props
+    return (
+      <Authenticator user={user}>
+        <div>
+          <NavBar user={user} notifications={notifications} />
+          {children}
+        </div>
+      </Authenticator>
+    )
   }
 })
 
-export default createContainer(() => ({ user: Meteor.user() }), Layout)
+export default createContainer(() => ({
+  user: Meteor.user(),
+  notifications: []
+}), Layout)
---
+++ imports/ui/navigation/notifications-bell.jsx
@@ -0,0 +1,58 @@
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
+import Dropdown from 'rebass/dist/Dropdown'
+import DropdownMenu from 'rebass/dist/DropdownMenu'
+import Button from 'rebass/dist/Button'
+
+const NotificationsBell = React.createClass({
+  propTypes: {
+    notifications: PropTypes.array
+  },
+
+  getInitialState () {
+    return { isDropdownOpen: false }
+  },
+
+  onBellClick () {
+    this.setState({ isDropdownOpen: true })
+  },
+
+  onDropdownDismiss () {
+    this.setState({ isDropdownOpen: false })
+  },
+
+  onSeeAllClick () {
+    this.setState({ isDropdownOpen: false })
+  },
+
+  onMarkAllReadClick () {
+    console.log('TODO: Mark all read')
+  },
+
+  render () {
+    return (
+      <Dropdown>
+        <Button backgroundColor='transparent' onClick={this.onBellClick}>🔔</Button>
+        <DropdownMenu open={this.state.isDropdownOpen} onDismiss={this.onDropdownDismiss}>
+          <div>
+            <div>2 unread notifications</div>
+            <Button backgroundColor='transparent' color='black' onClick={this.onMarkAllReadClick}>Mark all as read</Button>
+          </div>
+          <ul>
+            <li className='unread'>
+              <b>Peter Jackson</b> logged coverage for <b>Amazon UK Seller Stories</b>
+              <time dateTime='2016-10-17T19:00'>Just Now</time>
+            </li>
+            <li>
+              <b>Mike Douglas</b> logged feedback for <b>Sketch 4</b>
+              <time dateTime='2016-10-17T18:55'>5 minutes ago</time>
+            </li>
+          </ul>
+          <Link to='/notifications' activeClassName='active' onClick={this.onSeeAllClick}>See all</Link>
+        </DropdownMenu>
+      </Dropdown>
+    )
+  }
+})
+
+export default NotificationsBell
