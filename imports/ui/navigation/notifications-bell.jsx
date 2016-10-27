ce338c61c7b1c6257b0560377be5ea1f2fc51aa6
Bernard Mordan <bernardmordan@gmail.com>
2016-10-27 10:30:30 +0100

    adds menu

--- imports/ui/navigation/notifications-bell.jsx
+++ imports/ui/navigation/notifications-bell.jsx
@@ -4,6 +4,7 @@ import Dropdown from 'rebass/dist/Dropdown'
 import DropdownMenu from 'rebass/dist/DropdownMenu'
 import Button from 'rebass/dist/Button'
 import { NotificationBell } from '../images/icons'
+import { Notifications } from '../users/notification'
 
 const NotificationsBell = React.createClass({
   propTypes: {
@@ -34,22 +35,10 @@ const NotificationsBell = React.createClass({
     return (
       <Dropdown>
         <Button backgroundColor='transparent' onClick={this.onBellClick}><NotificationBell /></Button>
-        <DropdownMenu open={this.state.isDropdownOpen} onDismiss={this.onDropdownDismiss}>
-          <div>
-            <div>2 unread notifications</div>
-            <Button backgroundColor='transparent' color='black' onClick={this.onMarkAllReadClick}>Mark all as read</Button>
+        <DropdownMenu open={this.state.isDropdownOpen} onDismiss={this.onDropdownDismiss} right>
+          <div className='max-width-sm'>
+            {Notifications({notifications})}
           </div>
-          <ul>
-            <li className='unread'>
-              <b>Peter Jackson</b> logged coverage for <b>Amazon UK Seller Stories</b>
-              <time dateTime='2016-10-17T19:00'>Just Now</time>
-            </li>
-            <li>
-              <b>Mike Douglas</b> logged feedback for <b>Sketch 4</b>
-              <time dateTime='2016-10-17T18:55'>5 minutes ago</time>
-            </li>
-          </ul>
-          <Link to='/notifications' activeClassName='active' onClick={this.onSeeAllClick}>See all</Link>
         </DropdownMenu>
       </Dropdown>
     )
@@ -57,3 +46,6 @@ const NotificationsBell = React.createClass({
 })
 
 export default NotificationsBell
+
+// dummy data in lieu of actual notifications
+const notifications = [{'avatar': 'https://s3.amazonaws.com/uifaces/faces/twitter/krasnoukhov/128.jpg', 'icon': 'FeedCoverageIcon', 'name': 'Grace Roob', 'notification': 'Et est aspernatur repudiandae vel sunt accusamus est placeat.', 'campaign': 'Cronin, Hettinger and Toy', 'time': '2016-06-20T00:49:36.163Z', 'read': false}, {'avatar': 'https://s3.amazonaws.com/uifaces/faces/twitter/shinze/128.jpg', 'icon': 'FeedCoverageIcon', 'name': 'Enid Heaney', 'notification': 'Aut dignissimos cum iste.', 'campaign': 'Yost Inc', 'time': '2016-02-06T12:13:24.040Z', 'read': false}, {'avatar': 'https://s3.amazonaws.com/uifaces/faces/twitter/marcomano_/128.jpg', 'icon': 'FeedCampaignIcon', 'name': 'Una Strosin', 'notification': 'Ratione vero voluptates.', 'campaign': 'Gottlieb Inc', 'time': '2016-01-05T22:17:41.075Z', 'read': false}, {'avatar': 'https://s3.amazonaws.com/uifaces/faces/twitter/wim1k/128.jpg', 'icon': 'FeedCampaignIcon', 'name': 'Ruthe Beatty', 'notification': 'Aliquid quis itaque consequatur voluptas.', 'campaign': 'Corwin - Dicki', 'time': '2015-12-02T14:00:45.239Z', 'read': false}, {'avatar': 'https://s3.amazonaws.com/uifaces/faces/twitter/mkginfo/128.jpg', 'icon': 'FeedNeedToKnowIcon', 'name': 'Breanne Nolan', 'notification': 'Sunt modi ducimus ut ut id expedita sunt ipsum.', 'campaign': 'Brekke, Kling and Kuhn', 'time': '2016-04-30T11:08:08.870Z', 'read': true}]
