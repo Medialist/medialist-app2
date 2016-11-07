6967928bf268dfbcb007a4fd5f6675aa65193f96
Bernard Mordan <bernardmordan@gmail.com>
2016-11-07 23:22:09 +0000

    finally working autocomplete

--- imports/ui/campaigns/autocomplete.jsx
+++ imports/ui/campaigns/autocomplete.jsx
@@ -3,59 +3,82 @@ import React, { PropTypes } from 'react'
 export default React.createClass({
   propTypes: {
     clients: PropTypes.array.isRequired,
-    name: PropTypes.string.isRequired,
     updateField: PropTypes.func.isRequired,
-    className: PropTypes.string
+    className: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired
   },
   getInitialState () {
     return {suggestions: []}
   },
-  onChange (evt) {
-    const { name, value } = evt.target
-    const suggestions = this.getSuggestions(value)
+  componentWillReceiveProps (props) {
+    const { getSuggestions } = this
+    const { name, value, updateField } = props
+    if (!value) return
+    const suggestions = getSuggestions(value)
     this.setState({suggestions})
-    this.props.updateField(name, value)
+    if (suggestions.length === 0) updateField(name, value.substring(0, value.length - 1))
   },
   getSuggestions (value) {
     return this.props.clients.filter((client) => {
-      return client.name.toLowerCase().slice(0, value.length) === value
+      return client.name.toLowerCase().slice(0, value.length) === value.toLowerCase()
     })
   },
-  selectClient (field, value) {
-    this.props.updateField(field, value.name)
-    this.setState({suggestions: []})
+  selectValue (value) {
+    const { name, updateField } = this.props
+    updateField(name, value.name)
+  },
+  resetField (value) {
+    value = value || ''
+    const { name, updateField } = this.props
+    updateField(name, value)
   },
   onFocus () {
-    const field = this.props.name
-    const value = ''
-    this.setState({suggestions: []})
-    this.props.updateField(field, value)
+    this.resetField()
+  },
+  onBlur () {
+    setTimeout(() => {
+      const { name, value, clients, updateField } = this.props
+      this.setState({suggestions: []})
+      const client = clients.filter((c) => c.name.toLowerCase() === value.toLowerCase())
+      if (client.length === 0) updateField(name, '')
+    }, 250)
   },
   render () {
-    const { onChange, selectClient, onFocus } = this
+    const { selectValue, onFocus, onBlur } = this
     const { suggestions } = this.state
     const {
       className,
       name,
       value
     } = this.props
+    const style = {
+      borderBottom: '1px dashed',
+      width: '33%'
+    }
 
     return (
-      <div>
+      <div className='relative'>
         <input
           className={className}
           name={name}
-          onChange={onChange}
           value={value}
+          placeholder='Client'
           autoComplete='off'
           onFocus={onFocus}
-        />
-        {suggestions.length > 0 && <ul className='block list-reset width-100 center'>
-          {suggestions.map((client, i) => {
-            return <li className='input-inline pointer hover-bg-blue center border-gray80 p2 mx2' key={client._id} onClick={selectClient.bind(null, name, client)} tabIndex={i}>{client.name}</li>
-          })}
+          onBlur={onBlur} />
+        {suggestions.length !== 0 &&
+          <ul className='absolute list-reset center width-100'>
+            {suggestions.map((client, i) => {
+              return <li
+                className='pointer bg-white center border-bottom border-gray80 py2 px4 my1 mx-auto shadow-2'
+                key={client._id}
+                onClick={selectValue.bind(null, client)}
+                tabIndex={i}
+                style={style}>{client.name}</li>
+            })}
+          </ul>
         }
-        </ul>}
       </div>
     )
   }
