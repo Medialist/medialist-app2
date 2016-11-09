97c01af07341d1f1804027c6843a06355bc94d84
Bernard Mordan <bernardmordan@gmail.com>
2016-11-09 22:47:46 +0000

    yep html layout

--- imports/ui/contacts/contacts-import-page.jsx
+++ imports/ui/contacts/contacts-import-page.jsx
@@ -1,2 +1,26 @@
 import React from 'react'
-export default () => <div><h1>Contacts Import Page</h1></div>
+import Topbar from '../navigation/topbar'
+
+export default React.createClass({
+  render () {
+    const style = {
+      borderStyle: 'dashed',
+      borderWidth: '4px'
+    }
+    return (
+      <div>
+        <Topbar backLinkText='cancel' />
+        <section className='mx-auto center py6' style={{width: `${window.innerWidth / 2}px`}}>
+          <h1 className='mt6'>Import your contacts</h1>
+          <h2 className='normal'>Select a CSV file or Excel spreadsheet below</h2>
+          <div className='border-gray80 bg-white mx-auto py6 width-100' style={style}>
+            <h3 className='pt2 normal'>Drag and drop your file here</h3>
+            <h3 className='pt2'>OR</h3>
+            <button className='btn bg-blue white pt2 mb4'>Select a file from your computer...</button>
+          </div>
+          <h4 className='normal' style={{lineHeight: '2rem'}}>Importing a CSV file or Excel spreadsheet allows you to add and update contacts stored in Medialist. If a contact email address or Twitter handle in your spreadsheet matches an existing contact, the contact will be updated with the mapped values from your spreadsheet. Otherwise, a new contact will be created.</h4>
+        </section>
+      </div>
+    )
+  }
+})
---
+++ imports/ui/navigation/topbar.jsx
@@ -0,0 +1,32 @@
+import React, { PropTypes } from 'react'
+import { browserHistory } from 'react-router'
+
+const Topbar = React.createClass({
+  propTypes: {
+    backLinkText: PropTypes.string,
+    onBackClick: PropTypes.func
+  },
+
+  getDefaultProps () {
+    return { backLinkText: 'Back' }
+  },
+
+  onBackClick () {
+    if (this.props.onBackClick) return this.props.onBackClick()
+    browserHistory.go(-1)
+  },
+
+  render () {
+    const { onBackClick } = this
+    const { backLinkText } = this.props
+    return (
+      <nav className='block bg-white mb4 flex items-center width-100'>
+        <div className='flex-auto'>
+          <span className='pointer inline-block p4' onClick={onBackClick}>{`◀ ${backLinkText}`}</span>
+        </div>
+      </nav>
+    )
+  }
+})
+
+export default Topbar
