3c82c8eb83259a886ee01f21ec6e741a8fd7bf53
Oli Evans <oli@zilla.org.uk>
2016-11-16 16:24:52 +0000

    Custom checkbox

--- imports/ui/tables/selectable-row.jsx
+++ imports/ui/tables/selectable-row.jsx
@@ -1,12 +1,12 @@
 import React, { PropTypes } from 'react'
 import classnames from 'classnames'
+import Checkbox from './checkbox'
 
 const SelectableRow = ({ selected, data, children, onSelectChange, className }) => (
   <tr className={classnames(className, 'hover-opacity-trigger', { active: selected })}>
     <td>
-      <input
+      <Checkbox
         className={classnames('hover-opacity-100', { 'opacity-0': !selected })}
-        type='checkbox'
         checked={selected}
         onChange={() => onSelectChange(data)}
       />
