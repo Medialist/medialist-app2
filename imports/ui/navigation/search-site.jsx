e92b946f8bf5785248836bb29c8c641b9bb405aa
Bernard Mordan <bernardmordan@gmail.com>
2016-11-17 13:03:37 +0000

    something like that

---
+++ imports/ui/navigation/search-site.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SearchGrayIcon } from '../images/icons'
+
+export default () => {
+  return (
+    <div className='bg-blue-lighter'>
+      <SearchGrayIcon />
+      <input />
+    </div>
+  )
+}
