2f92433029df820420efe953a04be38df87a7948
Oli Evans <oli@zilla.org.uk>
2016-11-18 18:08:31 +0000

    Rebuild the activity filter

--- imports/ui/dashboard/activity-filters.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { AllTypesIcon, FeedCoverageIcon, FeedNeedToKnowIcon } from '../images/icons'
-
-export const TopActivityFilter = () => <span><AllTypesIcon /><span className='ml2'>Top Activity</span></span>
-export const CoverageFilter = () => <span><FeedCoverageIcon /><span className='ml2'>Coverage</span></span>
-export const NeedToKnowFilter = () => <span><FeedNeedToKnowIcon /><span className='ml2'>Need-to-knows</span></span>
