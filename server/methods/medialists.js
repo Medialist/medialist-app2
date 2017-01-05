458be0f08ac3c75a9f5fe2f8464040fa126dcf6d
Richard Silverton <silverton.richard@googlemail.com>
2017-01-05 12:35:49 +0000

    Selects correct fields from medialist doc

--- server/methods/medialists.js
+++ server/methods/medialists.js
@@ -56,7 +56,7 @@ Meteor.methods({
     if (!this.userId) throw new Meteor.Error('Only a logged-in user can (un)favourite a medialist')
     const user = Meteor.users.findOne(this.userId, { fields: { profile: 1 } })
     check(medialistSlug, String)
-    const medialist = Medialists.findOne({ slug: medialistSlug }, { fields: { image: 1, slug: 1, name: 1 }})
+    const medialist = Medialists.findOne({ slug: medialistSlug }, { fields: { image: 1, slug: 1, name: 1, client: 1 }})
     if (!medialist) throw new Meteor.Error('Cannot find medialist')
 
     if (user.profile.medialists.some((m) => m._id === medialist._id)) {
