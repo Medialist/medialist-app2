6c33eac1762db531e9fda17065f43aac16722918
Richard Silverton <silverton.richard@googlemail.com>
2017-01-05 13:28:47 +0000

    Tweaks linting

--- imports/methods/client/medialists.js
+++ imports/methods/client/medialists.js
@@ -5,7 +5,7 @@ Meteor.methods({
   'medialists/toggle-favourite': function (medialistSlug) {
     const user = Meteor.user()
     if (!this.userId) throw new Meteor.Error('Only a logged-in user can (un)favourite a medialist')
-    const medialist = Medialists.findOne({ slug: medialistSlug }, { fields: { image: 1, slug: 1, name: 1, client: 1 }})
+    const medialist = window.Medialists.findOne({ slug: medialistSlug }, { fields: { image: 1, slug: 1, name: 1, client: 1 } })
     if (!medialist) throw new Meteor.Error('Cannot find medialist')
 
     if (user.profile.medialists.some((m) => m._id === medialist._id)) {
