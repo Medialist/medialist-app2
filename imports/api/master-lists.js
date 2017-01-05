625a260466f0ac7ad72a4a21e678a7a24c8bfbd8
Richard Silverton <silverton.richard@googlemail.com>
2017-01-05 15:59:47 +0000

    Fixes import bug

--- imports/api/master-lists.js
+++ imports/api/master-lists.js
@@ -1,4 +1,4 @@
-import { Meteor } from 'meteor'
+import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 import { SimpleSchema } from 'meteor/aldeed:simple-schema'
 import nothing from './nothing'
