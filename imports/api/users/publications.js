9770776134b0886a6290c6d358e7c7e8c510eb94
Alan Shaw <alan@tableflip.io>
2017-01-09 14:58:21 +0000

    WIP edit team

---
+++ imports/api/users/publications.js
@@ -0,0 +1,61 @@
+import { Meteor } from 'meteor/meteor'
+import { check, Match } from 'meteor/check'
+import escapeRegExp from 'lodash.escaperegexp'
+
+const DEFAULT_LIMIT = 50
+
+Meteor.publish('users', function (opts = {}) {
+  if (!this.userId) return this.ready()
+
+  check(opts, {
+    term: Match.Optional(String),
+    limit: Match.Optional(Number)
+  })
+
+  let query = { _id: { $ne: this.userId } }
+  const options = {
+    fields: {
+      'profile.name': 1,
+      'services.twitter.profile_image_url_https': 1
+    },
+    sort: { createdAt: -1 },
+    limit: opts.limit || DEFAULT_LIMIT
+  }
+
+  if (opts.term) {
+    const regex = new RegExp(`${escapeRegExp(opts.term)}`, 'gi')
+
+    query = { $or: [
+      { 'profile.name': regex },
+      { 'services.twitter.screenName': regex }
+    ] }
+  }
+/*
+else {
+    // By default, show users in the same teams as me
+    const weightedUserIds = Medialists
+      .find(
+        { 'team._id': this.userId },
+        { fields: { team: 1 }, sort: { updatedAt: -1 }, limit: 10 }
+      )
+      .fetch()
+      .reduce((userIds, medialist) => {
+        (medialist.team || []).forEach((user) => {
+          userIds[user._id] = userIds[user._id] || 0
+          userIds[user._id]++
+        })
+        return userIds
+      }, {})
+
+    const $or = Object.keys(weightedUserIds)
+      .map((id) => ({ id, count: weightedUserIds[id] }))
+      .sort((a, b) => a.count - b.count)
+      .map(({ id }) => ({ 'team._id': id }))
+
+    query.$or.push({})
+    query.$or = $or
+  }
+*/
+
+  return Meteor.users.find(query, options)
+})
--- imports/ui/campaigns/edit-team.jsx
+++ imports/ui/campaigns/edit-team.jsx
@@ -1,44 +1,117 @@
 import React, { PropTypes } from 'react'
 import { Meteor } from 'meteor/meteor'
+import { ReactMeteorData } from 'meteor/react-meteor-data'
+import escapeRegExp from 'lodash.escaperegexp'
 import { SearchBlueIcon } from '../images/icons'
 import Modal from '../navigation/modal'
+import Loading from '../lists/loading'
 
 const EditTeam = React.createClass({
+  mixins: [ReactMeteorData],
+
   propTypes: {
-    open: PropTypes.bool.isRequired,
-    onDismiss: PropTypes.func.isRequired,
-    campaign: PropTypes.object
+    campaign: PropTypes.object.isRequired,
+    onDismiss: PropTypes.func.isRequired
   },
 
   getInitialState () {
     const { campaign } = this.props
-    return { team: campaign.team || [] }
+    return { term: '', team: (campaign.team || []).slice() }
   },
 
-  onSubmit (evt) {
-    evt.preventDefault()
-    this.props.onDismiss()
+  getMeteorData () {
+    const { term } = this.state
+    const subs = [ Meteor.subscribe('users', { term }) ]
+    const loading = !subs.every((sub) => sub.ready())
+
+    let query = {}
+    const options = {
+      fields: {
+        'profile.name': 1,
+        'services.twitter.profile_image_url_https': 1
+      },
+      sort: { createdAt: -1 }
+    }
+
+    if (term) {
+      const regex = new RegExp(`${escapeRegExp(term)}`, 'gi')
+
+      query = { $or: [
+        { 'profile.name': regex },
+        { 'services.twitter.screenName': regex }
+      ] }
+    }
+
+    return { users: Meteor.users.find(query, options), loading }
   },
 
-  onReset () {
+  onSearchTermChange (term) {
+    this.setState({ term })
+  },
+
+  // Determine if the passed user is a member of the team
+  isMember (user) {
+    const { team } = this.state
+    return (team || []).some((u) => u._id === user._id)
+  },
+
+  // Convert to a user ref
+  toMember (user) {
+    return {
+      _id: user._id,
+      name: user.profile.name,
+      avatar: user.services.twitter.profile_image_url_https
+    }
+  },
+
+  onToggleUser (user) {
+    const { team } = this.state
+
+    this.setState({
+      team: this.isMember(user)
+        ? team.filter((m) => m._id === user._id)
+        : team.concat(this.toMember(user))
+    })
+  },
+
+  onSubmit (e) {
+    e.preventDefault()
+    // TODO: update campaign
+  },
+
+  onReset (e) {
+    e.preventDefault()
     this.props.onDismiss()
   },
 
   render () {
-    const { open } = this.props
-    if (!open) return null
+    const { onSearchTermChange, onToggleUser, isMember, onSubmit, onReset } = this
+    const { term, team } = this.state
+    const { users, loading } = this.data
 
-    const { onSubmit, onReset, onSearch } = this
+    let content
+
+    if (!loading && !users.length) {
+      content = <UsersListEmpty />
+    } else {
+      content = (
+        <div>
+          <SearchBox
+            term={term}
+            onTermChange={onSearchTermChange} />
+          <SearchResults
+            users={users}
+            isMember={isMember}
+            onToggleUser={onToggleUser}
+            loading={loading} />
+        </div>
+      )
+    }
 
     return (
       <form onSubmit={onSubmit} onReset={onReset}>
-        <div className='border-gray60 border-bottom'>
-          <h1 className='f-xl normal center pt4 pb4'>Add Team Mates to this Campaign</h1>
-        </div>
-        <div className='py3 pl3 flex border-gray60 border-bottom'>
-          <SearchBlueIcon className='flex-none' />
-          <input className='flex-auto f-lg pa2 mx2' placeholder='Find team mates' onChange={onSearch} style={{ outline: 'none' }} />
-        </div>
+        <Header team={team} />
+        {content}
         <div className='p4 right-align'>
           <button className='btn bg-transparent blue mr2 left' type='button' disabled>Manage Team Mates</button>
           <button className='btn bg-transparent gray40 mr2' type='reset'>Cancel</button>
@@ -46,6 +119,54 @@ const EditTeam = React.createClass({
         </div>
       </form>
     )
+  }
+})
+
+const Header = () => (
+  <div className='border-gray60 border-bottom'>
+    <h1 className='f-xl normal center pt4 pb4'>Add Team Mates to this Campaign</h1>
+  </div>
+)
+
+const UsersListEmpty = () => <div>No users :(</div>
+
+const SearchBox = React.createClass({
+  propTypes: {
+    term: PropTypes.string.isRequired,
+    onTermChange: PropTypes.func.isRequired
+  },
+
+  render () {
+    return (
+      <div className='py3 pl3 flex border-gray60 border-bottom'>
+        <SearchBlueIcon className='flex-none' />
+        <input className='flex-auto f-lg pa2 mx2' placeholder='Find team mates' onChange={this.props.onTermChange} style={{ outline: 'none' }} value={this.props.term} />
+      </div>
+    )
+  }
+})
+
+const SearchResults = React.createClass({
+  propTypes: {
+    users: PropTypes.arrayOf(
+      PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        avatar: PropTypes.string
+      })
+    ),
+    isMember: PropTypes.func,
+    loading: PropTypes.bool,
+    onToggleUser: PropTypes.func.isRequired
+  },
+
+  getDefaultProps () {
+    return { isMember: () => false, loading: false }
+  },
+
+  render () {
+    const { loading } = this.props
+    if (loading) return <Loading />
   }
 })
 
