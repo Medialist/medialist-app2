3196c99bf0fc9297ab5487f1ae297a6fb9eb8147
Oli Evans <oli@zilla.org.uk>
2017-01-12 12:46:30 +0000

    Fix campaign view nav link color

--- imports/ui/navigation/nav-link.jsx
+++ imports/ui/navigation/nav-link.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default function (props) {
-  return <Link {...props} className='inline-block pointer p4 semibold f-sm gray-40' activeClassName='shadow-inset-blue blue' />
+  return <Link {...props} className='inline-block pointer p4 semibold f-sm gray40' activeClassName='shadow-inset-blue blue' />
 }
