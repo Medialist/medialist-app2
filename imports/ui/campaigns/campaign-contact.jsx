4ba35d62a627abc48234942a41b416391796b7f4
Alan Shaw <alan@tableflip.io>
2017-01-17 14:05:43 +0000

    refs #108 Add status dot to campaign contacts

--- imports/ui/campaigns/campaign-contact.jsx
+++ imports/ui/campaigns/campaign-contact.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 import { CircleAvatar } from '../images/avatar'
+import StatusDot from '../feedback/status-dot'
 
-const CampaignContact = (props) => (
-  <div className='pt3' style={{lineHeight: 1.3}}>
-    <Link to={`contact/${props.slug}`}>
-      <CircleAvatar className='inline-block' size={38} avatar={props.avatar} name={props.name} />
-      <div className='inline-block align-top pl3' style={{width: 220, height: 55}}>
-        <div className='f-md semibold gray10 truncate'>{props.name}</div>
-        <div className='f-sm normal gray20 truncate'>{(props.outlets && props.outlets.length) ? props.outlets[0].value : null}</div>
-        <div className='f-sm normal gray40 truncate'>{props.outlets.map((o) => o.label).join(', ')}</div>
-      </div>
-    </Link>
-  </div>
-)
+const CampaignContact = ({ contact, campaign }) => {
+  const status = campaign.contacts && campaign.contacts[contact.slug]
+  return (
+    <div className='pt3' style={{lineHeight: 1.3}}>
+      <Link to={`contact/${contact.slug}`}>
+        <CircleAvatar className='inline-block' size={38} avatar={contact.avatar} name={contact.name} />
+        <div className='inline-block align-top pl3' style={{width: 220, height: 55}}>
+          <div className='f-md semibold gray10 truncate'>
+            {contact.name}
+            <StatusDot name={status} className='ml1' />
+          </div>
+          <div className='f-sm normal gray20 truncate'>{(contact.outlets && contact.outlets.length) ? contact.outlets[0].value : null}</div>
+          <div className='f-sm normal gray40 truncate'>{contact.outlets.map((o) => o.label).join(', ')}</div>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+CampaignContact.propTypes = {
+  contact: PropTypes.object.isRequired,
+  campaign: PropTypes.object.isRequired
+}
 
 export default CampaignContact
---
+++ imports/ui/feedback/status-dot.jsx
@@ -0,0 +1,20 @@
+import React, { PropTypes } from 'react'
+import values from 'lodash.values'
+import classNames from 'classnames'
+
+const StatusDot = ({ name, size = 12, className }) => {
+  if (!name) return null
+  const ref = name.toLowerCase().replace(' ', '-')
+  className = classNames(`inline-block align-middle circle bg-${ref}`, className)
+  return (
+    <span title={name} className={className} style={{ width: size, height: size }} />
+  )
+}
+
+StatusDot.propTypes = {
+  name: PropTypes.oneOf(values(window.Contacts.status)).isRequired,
+  size: PropTypes.number,
+  className: PropTypes.string
+}
+
+export default StatusDot
