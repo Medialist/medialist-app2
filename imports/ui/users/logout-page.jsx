3bb5ab414e87b606dac9ebab7da99b446b8eb73a
Bernard Mordan <bernardmordan@gmail.com>
2017-01-19 16:20:23 +0000

    console.errors any errors

--- imports/ui/users/logout-page.jsx
+++ imports/ui/users/logout-page.jsx
@@ -4,7 +4,10 @@ import { Meteor } from 'meteor/meteor'
 
 const LogoutPage = React.createClass({
   componentDidMount () {
-    Meteor.logout(() => this.props.router.push('/'))
+    Meteor.logout((err) => {
+      if (err) console.error(err)
+      this.props.router.push('/')
+    })
   },
   render () {
     return null
