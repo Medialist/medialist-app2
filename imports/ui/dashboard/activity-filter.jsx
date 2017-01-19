cdcdf5506d13fc8b311eba70446adb68131f2561
Bernard Mordan <bernardmordan@gmail.com>
2017-01-19 17:42:25 +0000

    some more

--- imports/ui/dashboard/activity-filter.jsx
+++ imports/ui/dashboard/activity-filter.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 import Dropdown from 'rebass/dist/Dropdown'
-import DropdownMenu from 'rebass/dist/DropdownMenu'
+import DropdownMenu from '../lists/dropdown-menu'
 import { AllTypesIcon, FeedCoverageIcon, FeedNeedToKnowIcon, ChevronDown } from '../images/icons'
 
 const dropdownStyle = Object.assign({}, { width: 180 })
@@ -54,7 +54,7 @@ const ActivityFilter = React.createClass({
               {selected}
               <ChevronDown />
             </span>
-            <DropdownMenu right style={dropdownStyle} open={open} onDismiss={toggleDropdown}>
+            <DropdownMenu right arrowPosition={'60%'} style={dropdownStyle} open={open} onDismiss={toggleDropdown}>
               <nav className='py1'>
                 {filterNames.map((filterName) => (
                   <Filter key={filterName} name={filterName} selected={filterName === selected} onClick={() => this.onLinkClick(filterName)} />)
