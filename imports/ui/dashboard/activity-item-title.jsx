3ea4f62d5444394d6b1b45f192552ffeea5ac7cc
Bernard Mordan <bernardmordan@gmail.com>
2017-01-30 14:48:28 +0000

    repositions status on feedback - needs more work

--- imports/ui/dashboard/activity-item-title.jsx
+++ imports/ui/dashboard/activity-item-title.jsx
@@ -29,14 +29,16 @@ const ActivityItemTitle = ({ item, currentUser }) => {
     return (
       <span className='flex' style={{height: 19}}>
         <YouOrName className='semibold flex-none' currentUser={currentUser} user={createdBy} />
-        <span className='flex-auto truncate'>
-          <span className='gray10 ml1'>logged feedback </span>
-          <span className='f-xxxs gray60 mx1'><ChevronRight /></span>
-          <Link className='semibold gray10' to={`/campaigns/${encodeURIComponent(medialists[0])}`}>{medialists[0]}</Link>
-          <span className='f-xxxs gray60 mx1'><ChevronRight /></span>
-          <span className='semibold gray10'>{contacts[0].name}</span>
+        <span className='flex-auto' style={{whiteSpace: 'nowrap'}}>
+          <span className='inline-block truncate align-middle'>
+            <span className='gray10 ml1'>logged feedback </span>
+            <span className='f-xxxs gray60 mx1'><ChevronRight /></span>
+            <Link className='semibold gray10' to={`/campaigns/${encodeURIComponent(medialists[0])}`}>{medialists[0]}</Link>
+            <span className='f-xxxs gray60 mx1'><ChevronRight /></span>
+            <span className='semibold gray10'>{contacts[0].name}</span>
+          </span>
+          <span className='inline-block align-middle'><Status status={status} /></span>
         </span>
-        <span className='flex-none self-end'><Status status={status} /></span>
       </span>
     )
   }
