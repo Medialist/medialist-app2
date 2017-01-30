c5ed71853ffc5004422d0d69f55d3269cf938c3f
Oli Evans <oli@zilla.org.uk>
2017-01-30 14:23:59 +0000

    Adjust validation banner css. Drop the Oops.

--- imports/ui/errors/validation-banner.jsx
+++ imports/ui/errors/validation-banner.jsx
@@ -2,5 +2,11 @@ import React from 'react'
 
 export default ({ error }) => {
   if (!error) return null
-  return <div className='absolute top-0 left-0 right-0 bg-yellow-lighter p2 bold' style={{zIndex: 1}}>Oops. {error}</div>
+  return (
+    <div
+      className='shadow-2 absolute top-0 left-0 right-0 px4 bg-yellow-lighter f-xs gray10 semibold'
+      style={{zIndex: 1, paddingTop: 16, paddingBottom: 17}}>
+      {error}
+    </div>
+  )
 }
