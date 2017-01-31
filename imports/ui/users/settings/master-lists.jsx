bedd75ec74120657d3757faf71e90682c9e0c392
Bernard Mordan <bernardmordan@gmail.com>
2017-01-31 16:49:22 +0000

    all ui states and interactions tackeled

--- imports/ui/users/settings/master-lists.jsx
+++ imports/ui/users/settings/master-lists.jsx
@@ -1,10 +1,11 @@
 import React, { PropTypes } from 'react'
-import { SectorIcon, MenuCampaignIcon, DeleteIcon } from '../../images/icons'
+import { SectorIcon, MenuCampaignIcon, DeleteIcon, FeedEditIcon } from '../../images/icons'
+import Tooltip from '../../navigation/tooltip'
 import insertRuler from './insert-ruler'
 
 const SettingsMasterLists = ({ masterlists }) => {
   return (
-    <article className='pt4'>
+    <section className='pt4'>
       <div className='flex justify-center my4'>
         <SectorIcon className='blue svg-icon-lg' />
       </div>
@@ -14,19 +15,63 @@ const SettingsMasterLists = ({ masterlists }) => {
       </div>
       {insertRuler()}
       <MasterLists masterlists={masterlists} />
-    </article>
+    </section>
   )
 }
 
-const EmptyMasterLists = (creating, onCreate) => {
+SettingsMasterLists.propTypes = {
+  user: PropTypes.object,
+  masterlists: PropTypes.array
+}
+
+export default SettingsMasterLists
+
+const EmptyMasterLists = ({creating, onCreate}) => {
   return (
-    <div className='flex flex-column justify-start items-center' style={{height: 200}}>
-      <MenuCampaignIcon className='blue svg-icon-lg mt4 mb3' />
-      <div className='mt3 mb1 center'>You have not created any Campaign Lists yet</div>
-      <div className='mb3 center blue underlined pointer' onClick={onCreate}>Create a Campaign List</div>
+    <div style={{height: 200}}>
+      {creating ? (
+        <CreateMasterListInput onCreate={onCreate} />
+      ) : (
+        <div className='flex flex-column justify-start items-center'>
+          <MenuCampaignIcon className='blue svg-icon-lg mt4 mb3' />
+          <div className='mt3 mb1 center'>You have not created any Campaign Lists yet</div>
+          <div className='mb3 center blue underlined pointer' onClick={onCreate}>Create a Campaign List</div>
+        </div>
+      )}
     </div>
   )
 }
+
+const CreateMasterListInput = React.createClass({
+  propTypes: {
+    onCreate: PropTypes.func.isRequired
+  },
+  getInitialState () {
+    return { value: '' }
+  },
+  onChange (e) {
+    const { value } = e.target
+    this.setState({ value })
+  },
+  componentDidMount () {
+    this.refs.input.focus()
+  },
+  render () {
+    const { onChange } = this
+    const { value } = this.state
+    const { onCreate } = this.props
+    return (
+      <div className='flex justify-start items-center p2 my1 border border-gray80 bg-gray90 gray60'>
+        <input ref='input' className='input max-width-sm ml2' value={value} onChange={onChange} />
+        <div className='flex-none ml4 right-align' style={{width: 20}}>0</div>
+        <MenuCampaignIcon className='flex-none ml2' />
+        <div className='flex-auto right-align'>
+          <button className='btn bg-completed white' disabled={value.length === 0} onClick={() => onCreate(value)}>Add List</button>
+        </div>
+      </div>
+    )
+  }
+})
 
 const MasterLists = React.createClass({
   propTypes: {
@@ -35,53 +80,83 @@ const MasterLists = React.createClass({
   getInitialState () {
     return {
       creating: false,
-      editing: false,
+      editing: null,
       masterlists: this.props.masterlists.reduce((keyValues, listItem) => {
         keyValues[listItem.slug] = listItem.name
         return keyValues
       }, {})
     }
   },
-  onCreate () {
+  onCreate (name) {
     this.setState({creating: true})
+    if (!name) return
+    console.log({ name })
+    this.setState({creating: false})
   },
   onChange (slug, value) {
-    if (slug === value) return
-    if (!this.state.editing) this.setState({editing: true})
     this.setState({masterlists: { [slug]: value }})
   },
   onUpdate (slug) {
-    console.log('update', { slug, name: this.state.medialists[slug] })
-    this.setState({editing: false})
+    this.setState({ editing: null })
+    console.log({ name: this.state.masterlists[slug] })
+  },
+  onKeyDown (slug, key) {
+    if (key === 'Enter' || key === 'Tab') this.onUpdate(slug)
+  },
+  isEditing (slug) {
+    this.setState({ editing: slug })
   },
   removeMasterList (slug) {
     console.log('remove', slug)
   },
+  componentDidUpdate () {
+    if (!this.state.editing) return
+    this.refs[this.state.editing].focus()
+  },
   render () {
-    const { removeMasterList, onChange, onUpdate, onCreate } = this
-    const { editing } = this.state
+    const { removeMasterList, onChange, onUpdate, onKeyDown, isEditing, onCreate } = this
+    const { editing, creating } = this.state
     const { masterlists } = this.props
 
     if (masterlists.length < 1) {
-      return (
-        <EmptyMasterLists onCreate={onCreate} />
-      )
+      return <EmptyMasterLists creating={creating} onCreate={onCreate} />
     } else {
       return (
         <div className='mx3 pb3'>
           <div className='flex justify-start align-middle p2 mb1'>
             <div className='bold flex-none'>Campaign Lists ({masterlists.length})</div>
-            <div className='flex-auto blue underline right-align pointer'>Add new Campaign List</div>
+            <div className='flex-auto blue underline right-align pointer' onClick={(e) => onCreate()}>Add new Campaign List</div>
           </div>
+          {creating && <CreateMasterListInput onCreate={onCreate} />}
           {
             masterlists.map((masterlist) => {
               const { name, slug, items } = masterlist
               return (
                 <div className='flex justify-start items-center p2 my1 border border-gray80 bg-gray90 gray60' key={name}>
-                  <input className='input max-width-sm ml2' defaultValue={this.state.masterlists[slug]} onChange={(e) => onChange(slug, e.target.value)} />
-                  <div className='flex-none mx3 right-align' style={{width: 10}}>{items}</div>
-                  <MenuCampaignIcon className='ml2 flex-none' />
-                  {editing ? <button className='btn bg-completed white mt3 mb2' onClick={() => onUpdate(slug)}>Save Changes</button> : <DeleteIcon className='flex-auto right-align mr2' onClick={() => removeMasterList(slug)} />}
+                  <input
+                    ref={slug}
+                    className='input max-width-sm ml2'
+                    defaultValue={this.state.masterlists[slug]}
+                    disabled={slug !== editing}
+                    onChange={(e) => onChange(slug, e.target.value)}
+                    onBlur={() => onUpdate(slug)}
+                    onKeyDown={(e) => onKeyDown(slug, e.key)} />
+                  <div className='flex-none ml4 right-align gray40' style={{width: 20}}>{items}</div>
+                  <MenuCampaignIcon className='ml2 flex-none gray60' />
+                  {
+                    editing === slug ? (
+                      <div className='flex-auto right-align'>
+                        <button className='btn bg-completed white' onClick={() => onUpdate(slug)}>Save Changes</button>
+                      </div>
+                    ) : (
+                      <div className='flex-auto right-align'>
+                        <Tooltip title='Edit List'>
+                          <FeedEditIcon className='mr2 gray60 hover-gray40' onClick={(e) => isEditing(slug)} />
+                        </Tooltip>
+                        <DeleteIcon className='mr4 gray60' onClick={() => removeMasterList(slug)} />
+                      </div>
+                    )
+                  }
                 </div>
               )
             })
@@ -91,10 +166,3 @@ const MasterLists = React.createClass({
     }
   }
 })
-
-SettingsMasterLists.propTypes = {
-  user: PropTypes.object,
-  masterlists: PropTypes.array
-}
-
-export default SettingsMasterLists
