bdc631f932ca4bc871a4901047c7064b92235932
Bernard Mordan <bernardmordan@gmail.com>
2017-02-28 16:55:22 +0000

    adds a new shade of orange

--- imports/ui/feedback/post-box-nav.jsx
+++ imports/ui/feedback/post-box-nav.jsx
@@ -28,7 +28,7 @@ export const CoverageTab = ({selected, onClick}) => (
 
 export const NeedToKnowTab = ({selected, onClick}) => (
   <Tab selected={selected} onClick={onClick} >
-    <FeedNeedToKnowIcon className={selected ? 'orange' : 'gray60'} />
+    <FeedNeedToKnowIcon className={selected ? 'tangerine' : 'gray60'} />
     <span style={style}>Need to Know</span>
   </Tab>
 )
