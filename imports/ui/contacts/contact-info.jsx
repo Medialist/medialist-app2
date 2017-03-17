264b7a1f152ee0a580ab9345d6630eaeced56456
Oli Evans <oli@zilla.org.uk>
2017-03-17 16:07:26 +0000

    Fix margin on social links

--- imports/ui/contacts/contact-info.jsx
+++ imports/ui/contacts/contact-info.jsx
@@ -99,7 +99,7 @@ const ContactInfo = React.createClass({
               <div className='f-sm gray10'>{(outlets && outlets.length) ? outlets[0].value : null}</div>
               <div className='f-sm gray10' style={{paddingTop: 2}}>{outlets.map((o) => o.label).join(', ')}</div>
             </div>
-            <div className='pt4'>{socials.map((social) => <SocialIcon {...social} />)}</div>
+            <div className='pt4'>{socials.map((social) => <SocialIcon {...social} className='mr2' />)}</div>
           </div>
         </div>
         <div className='clearfix p3 pt4 mt4 border-gray80 border-bottom'>
