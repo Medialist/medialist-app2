4b016cb4b2d13c4b43e9194c526ce928480b9003
achingbrain <alex@achingbrain.net>
2017-04-20 16:24:04 +0100

    Add post migrations

--- imports/startup/server/migrations.js
+++ imports/startup/server/migrations.js
@@ -1,9 +1,10 @@
 import { Meteor } from 'meteor/meteor'
 import embeds from '/imports/api/embeds/embeds'
+import posts from '/imports/api/posts/posts'
 
 Migrations.add({
   version: 1,
-  name: 'Normalise embeds',
+  name: 'Normalise embeds and posts',
   up: () => {
     embeds.find().forEach(embed => {
       const update = {}
@@ -21,10 +22,42 @@ Migrations.add({
       }
 
       if (Object.keys(update).length) {
-        console.info(`Updating ${embed._id} ${Object.keys(update)}`)
+        console.info(`Updating embed ${embed._id} ${Object.keys(update)}`)
 
         embeds.update({
           _id: embed._id
+        }, {
+          $set: update
+        })
+      }
+    })
+
+    posts.find().forEach(post => {
+      const update = {}
+
+      if (Array.isArray(post.embed)) {
+        const embeds = []
+        let updateEmbeds = false
+
+        post.embeds.forEach((embed, index) => {
+          embeds[index] = embed
+
+          if (Array.isArray(embed.image)) {
+            updateEmbeds = true
+            embeds[index].image = embed.image[0]
+          }
+        })
+
+        if (updateEmbeds) {
+          update.embeds = embeds
+        }
+      }
+
+      if (Object.keys(update).length) {
+        console.info(`Updating post ${post._id} ${Object.keys(update)}`)
+
+        posts.update({
+          _id: post._id
         }, {
           $set: update
         })
