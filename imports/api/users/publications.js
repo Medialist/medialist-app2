c294d624e7c2f18c92ca69b26ad49a90bc434c10
Oli Evans <oli@zilla.org.uk>
2017-06-26 15:34:31 +0100

    Move schema from user publications

--- imports/api/users/publications.js
+++ imports/api/users/publications.js
@@ -1,10 +1,6 @@
 import { Meteor } from 'meteor/meteor'
 import { check, Match } from 'meteor/check'
 import escapeRegExp from 'lodash.escaperegexp'
-import { SimpleSchema } from 'meteor/aldeed:simple-schema'
-import { CampaignRefSchema } from '/imports/api/campaigns/schema'
-import { ContactRefSchema } from '/imports/api/contacts/schema'
-import { IdSchema } from '/imports/lib/schema'
 
 const DEFAULT_LIMIT = 2000
 
@@ -97,86 +93,6 @@ Meteor.publish('users-by-id', function (opts = {}) {
   }
   return Meteor.users.find(query, options)
 })
-
-export const UserProfileSchema = new SimpleSchema({
-  name: {
-    type: String,
-    optional: true
-  },
-  avatar: {
-    type: String,
-    optional: true
-  }
-})
-
-export const UserSchema = new SimpleSchema([
-  IdSchema, {
-    username: {
-      type: String,
-      optional: true
-    },
-    emails: {
-      type: Array,
-      optional: true
-    },
-    'emails.$': {
-      type: Object
-    },
-    'emails.$.address': {
-      type: String,
-      regEx: SimpleSchema.RegEx.Email
-    },
-    'emails.$.verified': {
-      type: Boolean,
-      optional: true
-    },
-    registered_emails: {
-      type: Array,
-      optional: true
-    },
-    'registered_emails.$': {
-      type: Object,
-      blackbox: true
-    },
-    createdAt: {
-      type: Date,
-      denyUpdate: true,
-      autoValue: function () {
-        if (this.isInsert) {
-          return new Date()
-        }
-
-        this.unset()
-      }
-    },
-    profile: {
-      type: UserProfileSchema,
-      optional: true
-    },
-    services: {
-      type: Object,
-      optional: true,
-      blackbox: true
-    },
-    heartbeat: {
-      type: Date,
-      optional: true
-    },
-    myContacts: {
-      type: [ContactRefSchema],
-      defaultValue: []
-    },
-    myCampaigns: {
-      type: [CampaignRefSchema],
-      defaultValue: []
-    },
-    onCampaigns: {
-      type: Number,
-      min: 0,
-      defaultValue: 0
-    }
-  }
-])
 
 export const createUser = (details) => {
   return Meteor.users.insert(details)
