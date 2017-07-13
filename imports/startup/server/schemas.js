5125aff2d287ce6a21a043fa6ad7ce0d3c37672a
achingbrain <alex@achingbrain.net>
2017-07-13 15:29:11 +0100

    Allow adding schemas on backend only

--- imports/startup/server/schemas.js
+++ imports/startup/server/schemas.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor'
 import attachSchemas from '/imports/lib/attach-schemas.js'
 
 Meteor.startup(() => {
-  if (Meteor.settings.public.attachSchemasToDatabase) {
+  if (Meteor.settings.attachSchemasToDatabase) {
     // attach schemas after any migrations have run
     attachSchemas()
   } else {
