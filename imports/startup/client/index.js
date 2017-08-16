b1d0bbed960888a6ae23ff903629be2ec656af78
achingbrain <alex@achingbrain.net>
2017-08-16 22:49:28 +0100

    Fix up browser tests

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,2 +1,6 @@
+// https://forums.meteor.com/t/solved-meteor-1-5-buffer-is-not-defined/36902/8
+import('buffer').then(({Buffer}) => {
+  global.Buffer = Buffer
+})
 import '/imports/startup/client/schemas'
 import '/imports/startup/client/react'
