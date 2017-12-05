5101566ee7c50eed4d22c829531bd8f2013849f8
Alan Shaw <alan@tableflip.io>
2017-12-05 15:32:48 +0000

    fixes autoRelease for edge cases

--- imports/ui/lists/data-dam.js
+++ imports/ui/lists/data-dam.js
@@ -1,25 +1,58 @@
 import { Meteor } from 'meteor/meteor'
 
-export const userRefAutoRelease = (field) => (data, diff) => {
+export const userRefAutoRelease = (field) => (data, diff, nextData) => {
+  // console.log('should autoRelease', diff)
   const byUser = (doc) => doc[field] && doc[field]._id === Meteor.userId()
 
   // If the current user added or updated some docs then release
   if (diff.added.some((byUser)) || diff.updated.some((byUser))) {
+    // console.log('YES: items added/updated by user')
     return true
   }
 
-  // Just release when deletions happen, there's no way to know that the
+  // Just release when removes happen, there's no way to know that the
   // logged in user deleted a thing since it is absent
   if (diff.total.removed) {
-    // Was this really a remove, or just an addition on a paginated list?
-    // If the latter, one was added to the front and it pushed one out from the back..
-    if (diff.total.added === 1 && diff.total.removed === 1 && diff.removed[0]._id === data[data.length - 1]._id) {
-      // Do not auto release
-    } else {
+    // ...but, was this really a remove, or an addition to a paginated list
+    // that forced a remove of an item at the back?
+    // [a, b, c] -> [x, a, b]
+
+    // If nothing was added then this really was a remove
+    if (!diff.total.added) {
+      // console.log('YES: items removed but nothing added')
+      return true
+    }
+
+    // So, things were added, but were all the removes from the end?
+    const endItems = data.slice(-diff.total.removed)
+    const removedFromEnd = endItems.every((endItem) => {
+      return diff.removed.some((item) => item._id === endItem._id)
+    })
+
+    // If they weren't all removed from the end, auto-release
+    if (!removedFromEnd) {
+      // console.log('YES: items NOT removed from the end')
       return true
     }
   }
 
+  if (diff.total.added) {
+    // So, things were added, were all the items added to the end?
+    // If so, then it's likely a remove has occurred previously and then
+    // an add has occurred to fill the space:
+    // [a, b, c] -> [b, c] -> [b, c, d]
+    const endItems = nextData.slice(-diff.total.added)
+    const addedToEnd = endItems.every((endItem) => {
+      return diff.added.some((item) => item._id === endItem._id)
+    })
+
+    if (addedToEnd) {
+      // console.log('YES: all the items added were added to the end')
+      return true
+    }
+  }
+
+  // console.log('NO')
   return false
 }
 
